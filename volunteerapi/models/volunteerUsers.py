07260407f3029cd158a36792d1865e3642d35b8d
Paolo Medel <paolomedel17@gmail.com>
2023-12-15 14:16:29 -0500

    model updated

--- volunteerapi/models/volunteerUsers.py
+++ volunteerapi/models/volunteerUsers.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class VolunteerUsers(models.Model):
     bio = models.CharField(max_length=2000)
-    profile_image_url = models.URLField(null=True, blank=True)
+    profile_image_url = models.CharField(null=True, blank=True, max_length=10000)
     created_on = models.DateTimeField(auto_now_add=True)
     is_business = models.BooleanField(default=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
